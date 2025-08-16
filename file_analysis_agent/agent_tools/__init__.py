b5a0a031cc5d37a8d554a6209d57005afe211542
Wasala <wasala@gmail.com>
2025-08-16 19:00:38 +0100

    Clean up test file

---
+++ file_analysis_agent/agent_tools/__init__.py
@@ -0,0 +1,21 @@
+from .tools import (
+    set,
+    top,
+    tail,
+    read_full_file,
+    find_within_doc,
+    get_random_lines,
+    get_file_metadata,
+    delete_cache,
+)
+
+__all__ = [
+    "set",
+    "top",
+    "tail",
+    "read_full_file",
+    "find_within_doc",
+    "get_random_lines",
+    "get_file_metadata",
+    "delete_cache",
+]
