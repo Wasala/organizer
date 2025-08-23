022c3afa356d022ddb963c491d92af782fa212fe
Wasala <wasala@gmail.com>
2025-08-23 15:03:42 +0100

    refactor planner init exports

--- file_organization_planner_agent/__init__.py
+++ file_organization_planner_agent/__init__.py
@@ -1,15 +1,10 @@
 """File organization planner agent package."""
 
-from .agent_tools import (
-    find_similar_file_reports,
-    append_organization_notes,
-    get_file_report,
-    target_folder_tree,
-)
+from . import agent_tools as _tools
 
-__all__ = [
-    "find_similar_file_reports",
-    "append_organization_notes",
-    "get_file_report",
-    "target_folder_tree",
-]
+find_similar_file_reports = _tools.find_similar_file_reports
+append_organization_notes = _tools.append_organization_notes
+get_file_report = _tools.get_file_report
+target_folder_tree = _tools.target_folder_tree
+
+__all__ = _tools.__all__
